17db920f5fdd3c251ee75491ff2cd13bbb3e542b
rkalesalad <rkaelle2@gmail.com>
2023-01-26 12:57:49 -0800

    build update

---
+++ build/precache-manifest.e952a0489835d533ded8e6b134032683.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0876fc1a88cb8a2248ea061a5990daa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "120b3a088bd77389cf54",
+    "url": "/static/css/2.f9257326.chunk.css"
+  },
+  {
+    "revision": "3864a5f15bc809d82fa2",
+    "url": "/static/css/main.d7c6c975.chunk.css"
+  },
+  {
+    "revision": "120b3a088bd77389cf54",
+    "url": "/static/js/2.ab41d55b.chunk.js"
+  },
+  {
+    "revision": "47c32a7219e776829212994a77f7e474",
+    "url": "/static/js/2.ab41d55b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3864a5f15bc809d82fa2",
+    "url": "/static/js/main.9803f483.chunk.js"
+  },
+  {
+    "revision": "ae73872696acd0a58e5c",
+    "url": "/static/js/runtime-main.55dd03d6.js"
+  }
+]);
