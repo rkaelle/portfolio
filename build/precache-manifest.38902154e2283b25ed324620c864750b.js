04ce366511e54cee303901e14f91e3001da977f2
rkalesalad <rkaelle2@gmail.com>
2023-01-31 14:53:56 -0800

    Typo build

---
+++ build/precache-manifest.38902154e2283b25ed324620c864750b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584f027c037cb173c07fedc2b9d3e4bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "120b3a088bd77389cf54",
+    "url": "/static/css/2.f9257326.chunk.css"
+  },
+  {
+    "revision": "7d5ad61804ba76ea3f41",
+    "url": "/static/css/main.ef7ad4ff.chunk.css"
+  },
+  {
+    "revision": "120b3a088bd77389cf54",
+    "url": "/static/js/2.ab41d55b.chunk.js"
+  },
+  {
+    "revision": "47c32a7219e776829212994a77f7e474",
+    "url": "/static/js/2.ab41d55b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5ad61804ba76ea3f41",
+    "url": "/static/js/main.28ec920b.chunk.js"
+  },
+  {
+    "revision": "ae73872696acd0a58e5c",
+    "url": "/static/js/runtime-main.55dd03d6.js"
+  }
+]);
