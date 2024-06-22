dbf871c27f71d2c79e5edeebe42faee5d5392cc0
rkalesalad <rkaelle2@gmail.com>
2024-06-22 02:40:58 -0700

    push build

--- src/App.js
+++ src/App.js
@@ -7,28 +7,12 @@ import "./App.css";
 import "./styles/Global.css";
 import "rsuite/dist/styles/rsuite-default.css";
 
+
 function App() {
-  const [scrollProgress, setScrollProgress] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-      const progress = (document.documentElement.scrollTop / totalHeight) * 100;
-      setScrollProgress(progress);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div className="App">
       <DefaultSeo {...seo.defaultSeo} />
       <SocialProfileJsonLd {...seo.socialProfileJsonLd} />
-      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '5px', backgroundColor: '#ccc' }}>
-        <div style={{ height: '5px', width: `${scrollProgress}%`, backgroundColor: 'blue' }}></div>
-      </div>
       <SidebarNav />
       <Main />
     </div>
