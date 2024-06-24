5acb5f1e0293cb2ed94100857b28bb92c412aca3
rkalesalad <rkaelle2@gmail.com>
2024-06-24 00:06:38 -0700

    update build

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Gallery from 'react-photo-gallery';
 import { photos } from '../components/photos';
+import "../styles/Gallery.css";
 
 const GalleryPage = () => {
-    return <Gallery photos={photos} direction={"column"} />;
+
+    return (
+        <Gallery 
+            photos={photos} 
+            direction={"column"} 
+            showFullscreenButton={true}
+            showGalleryFullscreenButton={true}
+            infinite={true}
+            // Optional: Pass state values as props if the Gallery component uses them
+        />
+    );
 };
 
 export default GalleryPage;
