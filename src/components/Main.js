c1e17002ffae8905a331146e6badaa457c28d36c
rkalesalad <rkaelle2@gmail.com>
2024-07-07 17:25:34 -0700

    properly added it to public assets

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,6 @@ import Manage from '../pages/manage';
 import Home from '../pages/Home';
 import GalleryPage from '../pages/gallery';
 import ErrorPage from '../pages/errorPage'; 
-import SignaturePage from '../pages/signaturePage'; 
 import ScrollIndicator from './ScrollIndicator';
 
 const Main = () => {
@@ -15,7 +14,6 @@ const Main = () => {
         <Route exact path='/' component={Home} />
         <Route exact path='/manage' component={Manage} />
         <Route exact path='/gallery' component={GalleryPage} />
-        <Route path="/signature" component={SignaturePage} />
         <Route component={ErrorPage} />
       </Switch>
     </Router>
--- src/pages/signaturePage.js
+++
@@ -1,6 +0,0 @@
-const SignaturePage = () => (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    <img src={require('../assets/Signature.gif')} alt="Signature" />
-  </div>
-);
-export default SignaturePage;
