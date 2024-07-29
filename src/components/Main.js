09f47f01778ec80e28f51f2fa0166348e0d8eca7
rkalesalad <rkaelle2@gmail.com>
2024-07-29 03:00:45 -0700

    switched to browserrouter for cleaner urls

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Manage from '../pages/manage';
 import Home from '../pages/Home';
